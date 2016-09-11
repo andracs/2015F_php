dba322eeb768e7766344a4eaf8cdc13747871a58
Andras Acs <acsandras@gmail.com>
2016-09-11 23:27:08 +0200

    Tiny improvements

--- ovelse1-hello-world/index.php
+++ ovelse1-hello-world/index.php
@@ -1,22 +1,24 @@
 <?php 
 
+// Hello world
+echo "Hello <strong>World</strong>! <br>"; 
+
 // Talvariablaer
 $love = 50;
 $antal_personer = 25;
 $love_paa_falster = $love * $antal_personer;
 
-print "Der er så meget LOVE på Falster i dag: " . $love_paa_falster;
+// Textvariabel (string)
+$by = "Nykøbing"; 
 
-  // Her kommer koden 
-  echo "Hello <strong>World</strong>! <br>"; 
-  
-  //Min første variable
- $color = "red";
- 
- echo "Min variable indeholder: $color" ;
- 
-print "Pølse";
+print "<p>Der er så meget LOVE i $by i dag: " . $love_paa_falster . "</p>";
 
+// Min første variable
+$color = "red";
+ 
+echo "<p>Min variable indeholder: $color</p>" ;
+ 
+print "<p><i>print() functionen er næsten det samme som echo(), men print kan returnere en værdi. (Kudos til <a href='http://snuki.me'>Snuki.me</a> for info)</i></p>";
 
   
 ?>
